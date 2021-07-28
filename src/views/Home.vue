afbad39c856b9d48a1500aaa93b9bdea963e3644
Juan Santiago <jsantiagovalerio@gmail.com>
2021-07-27 22:31:12 -0500

    hola vuejs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="home">
       <img alt="Vue logo" src="../assets/logo.png" />
-      <HelloWorld msg="Welcome to Your Vue.js App" />
-      <h1>{{ hola }}</h1>
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+      <h1>Hola desde Vuejs</h1>
    </div>
 </template>
 
